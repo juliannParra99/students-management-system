f7fa7e91f9abf78a382995f0df4510df39edfea5
julianParrax <julianfranco.parra@gmail.com>
2023-11-08 21:36:15 -0300

    Added: 1. we use module 'mysql'to put the data necesary to get acces(later)to the database + running server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
-const express = require('express'); 
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const mysql = require("mysql");
 
-app.listen(8081, ()=>{
-    console.log("Server is running on port: ", 8081);
-} )
+app.use(cors());
+
+// Configura los parámetros de conexión a tu base de datos
+const db = mysql.createConnection({
+  host: "localhost", // Host de la base de datos
+  user: "root", // Usuario de la base de datos
+  password: "123456789river", // Contraseña del usuario
+  database: "students_management", // Nombre de la base de datos
+});
+
+
+app.get("/", (req,res) => {
+    res.json("hey from the server")
+});
+
+app.listen(8081, () => {
+  console.log("Server is running on port: ", 8081);
+});
