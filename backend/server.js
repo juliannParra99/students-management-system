b58dab37136535f0689191e3811e62417b4b2608
julianParrax <julianfranco.parra@gmail.com>
2023-11-09 21:11:30 -0300

    Added: 2. We made the initial component to show the data from the backend (component students) + connection between server and DB + error handle in the backend if the conection with the db fails + show those objects from the db  in the console in the browser

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 app.use(cors());
 
@@ -13,9 +13,26 @@ const db = mysql.createConnection({
   database: "students_management", // Nombre de la base de datos
 });
 
+app.get("/", (req, res) => {
+  // SQL query to select all columns from the "students" table
+  const sqlGet = "SELECT * FROM students";
 
-app.get("/", (req,res) => {
-    res.json("hey from the server")
+  // Execute the SQL query using the database connection (db)
+  db.query(sqlGet, (error, data) => {
+    // Check if there is an error in executing the SQL query: Error handle
+    if (error) {
+      // Log the error details to the console
+      console.error("Error executing query:", error);
+
+      // Send a JSON response with the error information and a 500 Internal Server Error status
+      return res
+        .status(500)
+        .json({ error: "Internal Server Error", details: error });
+    } else {
+      // Send a JSON response with the retrieved data and a 200 OK status
+      res.status(200).json(data);
+    }
+  });
 });
 
 app.listen(8081, () => {
