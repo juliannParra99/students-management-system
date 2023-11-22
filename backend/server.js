99f813713dabe71cb9de49e3aae042404c02b129
julianParrax <julianfranco.parra@gmail.com>
2023-11-21 21:20:29 -0300

    Added: 14. Added middleware to handle static files from server if it's necessary + configuration of port to handle if we use the server from the localhost or from other domain in which we have our website online + built-in middleware; importat: it would be useful to have a folder to organize all my middlewares, create a whitlist where i can have control of those domaing which access to my server. And additionaly, investigate more about  THIRD PARTY MIDDLEWARE

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require('path');
 const studentsRoutes = require("./routes/studentsRoutes");
 
 
+const PORT = process.env.PORT || 8081;
 app.use(cors());
 app.use(express.json());
+// built-in middleware to handle urlencoded data
+// in other words, form data:  
+// ‘content-type: application/x-www-form-urlencoded’
+app.use(express.urlencoded({ extended: false }));
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
 //use the modules in the router folder
 app.use("/", studentsRoutes)
 
+// Obtener el puerto de la variable de entorno o usar uno por defecto; util para usar otro puerto si lo queremos desplegar.
 
 
-app.listen(8081, () => {
-  console.log("Server is running on port: ", 8081);
+app.listen(PORT, () => {
+  console.log("Server is running on port: ", PORT);
 });
